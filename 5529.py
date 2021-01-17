5b0e2a2888a81648a5b042e71975d1a196d819fa
zhangdavids <zhangjohn202@gmail.com>
2021-01-17 14:17:51 +0800

    fixed

--- 5529.py
+++ 5529.py
@@ -1,9 +1,51 @@
 from typing import List
 
+
 class Solution:
     def canMouseWin(self, grid: List[str], catJump: int, mouseJump: int) -> bool:
 
+        n, m = len(grid), len(grid[0])
+        for i in range(n):
+            for j in range(m):
+                # 猫
+                if grid[i][j] == 'C':
+                    cp = (i, j)
+                # 老鼠
+                if grid[i][j] == 'M':
+                    mp = (i, j)
+                # 食物
+                if grid[i][j] == 'F':
+                    fp = (i, j)
 
-        def dfs():
-            pass
-        
+        f = {}
+
+        def work(status):
+            cp, mp, r, steps = status
+            if cp == fp or steps == 40 or cp == mp:
+                return r == 1
+            elif mp == fp:
+                return r == 0
+            if status not in f:
+                n, m = len(grid), len(grid[0])
+                if r == 0:
+                    x, y = mp
+                    jp = mouseJump
+                else:
+                    x, y = cp
+                    jp = catJump
+                for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    for i in range(jp + 1):
+                        xx, yy = x + dx * i, y + dy * i
+                        if 0 <= xx < n and 0 <= yy < m and grid[xx][yy] != '#':
+                            nw = (
+                                cp if r == 0 else (xx, yy), mp if r == 1 else (xx, yy), r ^ 1,
+                                steps + (0 if r == 0 else 1))
+                            if not work(nw):
+                                f[status] = True
+                                return True
+                        else:
+                            break
+                f[status] = False
+            return f[status]
+
+        return work((cp, mp, 0, 0))
--- 5653.py
+++ 5653.py
@@ -3,13 +3,13 @@ from typing import List
 
 class Solution:
     def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
-        a=[min(i) for i in rectangles]
+        a = [min(i) for i in rectangles]
         # print(a)
-        cnt= max(a)
+        cnt = max(a)
         ans = 0
         for item in a:
             if item == cnt:
-                ans +=1
+                ans += 1
 
         return ans
 
