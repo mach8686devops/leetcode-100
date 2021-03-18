2800f93b18fa7052c7e3c60d9406d63dffe2922a
zhangdavids <zhangjohn202@gmail.com>
2021-03-18 16:08:56 +0800

    fixed

--- 0887.py
+++ 0887.py
@@ -1,4 +1,13 @@
 class Solution:
+    def superEggDrop3(self, K: int, N: int) -> int:
+        dp = [[0] * (K + 1) for _ in range(N + 1)]
+        m = 0
+        while dp[m][K] < N:
+            m += 1
+            for i in range(1, K + 1):
+                dp[m][i] = dp[m - 1][i - 1] + 1 + dp[m - 1][i]
+        return m
+
     def superEggDrop2(self, K: int, N: int) -> int:
         ans, dp = 0, [0 for _ in range(K + 1)]
         while dp[K] < N:
